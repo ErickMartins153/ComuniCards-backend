cb0bdcddb4cf284fa2e6305cd128e17a43063de1
ErickMartins153 <121458723+ErickMartins153@users.noreply.github.com>
2024-09-22 18:26:56 -0300

    primeiro commit

---
+++ src/main/java/br/upe/comunicards/ComunicardsApplication.java
@@ -0,0 +1,13 @@
+package br.upe.comunicards;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ComunicardsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ComunicardsApplication.class, args);
+	}
+
+}
